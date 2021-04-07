189593d6cb070aeed5342bdb21f636020743d815
Niklas Bundschuh <niklas.bundschuh@kurtzersa.de>
2021-04-07 14:47:41 +0200

    Erster versuch kontakt mit der SPS aufzunehmen. snap7.client.Client() funktioniert nicht. Es kommt immer die fehlermeldung das die snap7 libary nicht installiert ist. Sonst noch nicht mit SPS ausprobiert

---
+++ ConnectionBetweenSPS.py
@@ -0,0 +1,23 @@
+import snap7.client 
+from snap7.util import *
+from snap7.types import *
+
+def ReadMemory(plc, byte, bit, datatype):
+    result = plc.read_area(areas['MK'], 0, byte, datatype)
+    value = 0
+    if datatype == S7WLBit:
+        return get_bool(result,0,1)
+    elif datatype == S7WLByte or datatype == S7WLWord:
+        return get_int(result,0)
+    elif datatype == S7WLReal:
+        return get_real(result,0) 
+    elif datatype == S7WLDWord:
+        return get_dword(result,0)
+    else:
+        return None
+
+if __name__=="__main__":
+    plc = snap7.client.Client()
+    plc.connect('10.10.54.2', 0,1) #maybe a other IP adress
+    print:(ReadMemory(plc, 100, 0, S7WLReal))
+    print:(ReadMemory (plc, 120, 0, S7WLWord))
